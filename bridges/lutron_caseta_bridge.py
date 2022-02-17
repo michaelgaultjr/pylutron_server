a72047922c3aefbb5f1de0234fec3a9374990238
THEGamingninja <thegamingninjamj@gmail.com>
2022-02-16 21:54:24 -0500

    Change logger variable names

--- bridges/lutron_caseta_bridge.py
+++ bridges/lutron_caseta_bridge.py
@@ -2,37 +2,39 @@ import logging
 from bridges.bridge import Bridge
 from pylutron_caseta.smartbridge import Smartbridge, LEAP_PORT
 
+LOG = logging.getLogger(__name__)
+
+
 class LutronCasetaBridge(Bridge):
-	__logger = logging.getLogger(__name__)
-	__bridge: Smartbridge = None
+    __bridge: Smartbridge = None
 
-	async def connect(
-			self,
-			host: str,
-			keyfile: str,
-			certfile: str,
-			ca_certs: str,
-			port: int = None
-		):
-		if (port is None): 
-			port = LEAP_PORT
+    async def connect(
+        self,
+        host: str,
+        keyfile: str,
+        certfile: str,
+        ca_certs: str,
+        port: int = None
+    ):
+        if (port is None):
+            port = LEAP_PORT
 
-		self.__bridge: Smartbridge = Smartbridge.create_tls(
-				hostname=host, 
-				port=port,
-				keyfile=keyfile,
-				certfile=certfile,
-				ca_certs=ca_certs,
-		)
-		await self.__bridge.connect()
+        self.__bridge: Smartbridge = Smartbridge.create_tls(
+            hostname=host,
+            port=port,
+            keyfile=keyfile,
+            certfile=certfile,
+            ca_certs=ca_certs,
+        )
+        await self.__bridge.connect()
 
-	@property
-	def is_connected(self):
-		return self.__bridge is not None and self.__bridge.is_connected
+    @property
+    def is_connected(self):
+        return self.__bridge is not None and self.__bridge.is_connected
 
-	async def activate_scene(self, scene_id: str):
-		await self.__bridge.activate_scene(scene_id)
-		self.__logger.debug("Activated scene: " + id)
+    async def activate_scene(self, scene_id: str):
+        await self.__bridge.activate_scene(scene_id)
+        LOG.debug("Activated scene: " + id)
 
-	def get_scenes(self):
-		return self.__bridge.get_scenes()
+    def get_scenes(self):
+        return self.__bridge.get_scenes()
--- bridges/mock_bridge.py
+++ bridges/mock_bridge.py
@@ -1,45 +1,47 @@
 import logging
 from bridges.bridge import Bridge
 
+LOG = logging.getLogger(__name__)
+
+
 class MockBridge(Bridge):
-	__logger = logging.getLogger(__name__)
 
-	async def connect(
-			self,
-			host: str,
-			keyfile: str,
-			certfile: str,
-			ca_certs: str,
-			port: int = None
-		):
-		self.__logger.debug({
-			"host": host,
-			"keyfile": keyfile,
-			"certfile": certfile,
-			"ca_certs": ca_certs,
-			"port": port
-		})
+    async def connect(
+        self,
+        host: str,
+        keyfile: str,
+        certfile: str,
+        ca_certs: str,
+        port: int = None
+    ):
+        LOG.debug({
+            "host": host,
+            "keyfile": keyfile,
+            "certfile": certfile,
+            "ca_certs": ca_certs,
+            "port": port
+        })
 
-	@property
-	def is_connected(self):
-		return True;
+    @property
+    def is_connected(self):
+        return True
 
-	async def activate_scene(self, scene_id: str):
-		self.__logger.debug(f"Activated scene: '{scene_id}'")
+    async def activate_scene(self, scene_id: str):
+        LOG.debug(f"Activated scene: '{scene_id}'")
 
-	def get_scenes(self):
-		# Mock Scenes
-		return { 
-			"scene1": { 
-				"id":"scene1", 
-				"name": "Scene 1"
-			}, 
-			"scene2": { 
-				"id":"scene2", 
-				"name": "Scene 2"
-			}, 
-			"scene3": { 
-				"id":"scene3", 
-				"name": "Scene 3" 
-			} 
-		}
+    def get_scenes(self):
+        # Mock Scenes
+        return {
+            "1": {
+                "scene_id": "1",
+                "name": "Scene 1"
+            },
+            "2": {
+                "scene_id": "2",
+                "name": "Scene 2"
+            },
+            "3": {
+                "scene_id": "3",
+                "name": "Scene 3"
+            }
+        }
